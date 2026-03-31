e4e9d9a76732b90fd931ade4afa45519f756116b
pidanov <spp9@tpu.ru>
2026-03-31 19:56:40 +0700

    attempt 2.5

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main{
         try {
             while (true) {
                 System.out.println("Проверка проекта");
-                Thread.sleep(1000);
+                Thread.sleep(1001);
             }
         } catch (InterruptedException e){
             System.out.println("Ошибка!");
